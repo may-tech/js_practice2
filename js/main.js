c51e880aa9070e1918e8a2b20b2062483398ffe8
may-tech <aehkloprsw@gmail.com>
2020-03-28 17:43:56 +0900

    [add] index.html, styles.css, main.jsの追加

---
+++ js/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const btn=document.getElementById('btn');
+
+btn.addEventListener('click',()=>{
+  const n=Math.floor(Math.random()*3);
+
+  switch(n){
+    case 0:
+      btn.textContent='大吉';
+      break;
+    case 1:
+      btn.textContent='吉';
+      break;
+    case 2:
+      btn.textContent='凶';
+      break;
+
+  }
+})
