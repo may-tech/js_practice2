90d80fcc0f4862c790dd87cdd5a7be6b0e198506
may-tech <aehkloprsw@gmail.com>
2020-03-28 17:57:54 +0900

    [clean] switch文を配列で書き換え

--- js/main.js
+++ js/main.js
@@ -3,18 +3,7 @@
 const btn=document.getElementById('btn');
 
 btn.addEventListener('click',()=>{
-  const n=Math.floor(Math.random()*3);
-
-  switch(n){
-    case 0:
-      btn.textContent='大吉';
-      break;
-    case 1:
-      btn.textContent='吉';
-      break;
-    case 2:
-      btn.textContent='凶';
-      break;
-
-  }
+  const results=['大吉','中吉','凶','末吉'];
+    btn.textContent=results[Math.floor(Math.random()*results.length)]; 
+  
 })
